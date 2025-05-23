635dd88781be115d3e18e54def46e8b4ab098ea1
Timmydrax <timmydrax@gmail.com>
2025-05-23 10:21:07 +0100

    CHORE: Little changes.

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
           <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         </div>
 
-        <section className="container mx-auto px-8">
+        <section className="container mx-auto px-4 lg:px-8">
           <Navbar />
           <Hero />
           <Technologies />
