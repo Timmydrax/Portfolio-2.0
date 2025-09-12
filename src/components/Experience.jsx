bf2543157d83fdfb98bd2ed76e01ac56f9aba06e
Timmydrax <timmydrax@gmail.com>
2025-09-12 16:29:04 +0100

    CHORE: Adjusted Animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,7 @@ const Experience = () => {
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: -100 }}
           transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
           className="my-20 text-center text-4xl"
         >
           Experience
@@ -21,6 +22,7 @@ const Experience = () => {
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 0.5 }}
+                viewport={{ once: true }}
                 className="w-full lg:w-1/4"
               >
                 <p className="mb-2 text-sm text-stone-400 dark:text-stone-400">
@@ -32,6 +34,7 @@ const Experience = () => {
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 0.75 }}
+                viewport={{ once: true }}
                 className="w-full max-w-xl lg:w-3/4"
               >
                 <h3 className="mb-2 font-semibold">
